641f295ba10d38c3fdf03620f0c207829d36cb9d
Dennis <dennis.drochmann@gmail.com>
2025-03-31 17:12:02 +0200

    update

--- script.user.js
+++ script.user.js
@@ -23,21 +23,11 @@
 
     async function createButton(container) {
 
-        // Lade die HTML-Resource
-        //const htmlText = GM_getResourceText("popupHtml");
-
-        // Erzeuge einen Blob daraus
-        //const blob = new Blob([htmlText], { type: "text/html" });
-
-        // Erzeuge eine URL, die man mit window.open verwenden kann
-        // popupUrl = URL.createObjectURL(blob);
         const popupUrl = "https://raw.githubusercontent.com/dev-101010/ctd-upgrade-tool/refs/heads/master/popup.html";
 
-        // Lade den HTML-Inhalt von GitHub
         const response = await fetch(popupUrl);
         const html = await response.text();
 
-        // Erzeuge einen echten HTML-BLOB mit dem richtigen MIME-Type
         const blob = new Blob([html], { type: "text/html" });
         const blobUrl = URL.createObjectURL(blob);
 
@@ -48,7 +38,7 @@
         button.addEventListener("click", () => {
             sessionStorage.setItem("userToken", userToken);
             sessionStorage.setItem("authToken", authToken);
-            window.open(blobUrl, "_blank", "width=600,height=400");
+            window.open(blobUrl, "_blank", "width=800,height=400");
         });
         container.appendChild(button);
     }
