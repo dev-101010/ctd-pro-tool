c64e8cd81b12a69734691741083eb4a4c944f972
Dennis <dennis.drochmann@gmail.com>
2025-03-31 05:29:02 +0200

    Initial commit

---
+++ script.user.js
@@ -0,0 +1,10 @@
+// ==UserScript==
+// @name         CTD Upgrade Tool
+// @namespace    https://github.com/dev-101010/ctd-upgrade-tool
+// @version      0.1
+// @description  CTD Tower upgrade
+// @author       dev-101010
+// @match        https://www.c-td.de/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=c-td.de
+// ==/UserScript==
+
