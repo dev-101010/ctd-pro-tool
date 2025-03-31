b2e627b221a75bf27baff0ba49c07f25b3128820
Dennis <dennis.drochmann@gmail.com>
2025-03-31 06:18:25 +0200

    update

--- script.user.js
+++ script.user.js
@@ -6,7 +6,7 @@
 // @author       dev-101010
 // @match        https://ctddev.shimly-dev.de/member/battlefield/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=c-td.de
-// @resource     popupHtml https://raw.githubusercontent.com/dev-101010/ctd-upgrade-tool/refs/heads/master/popup.html
+// @resource     popupHtml https://raw.githubusercontent.com/dev-101010/ctd-upgrade-tool/master/popup.html
 // @grant        GM_getResourceText
 // ==/UserScript==
 
